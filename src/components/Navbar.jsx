d31ed1a454b1dec06aa8a1677cfe55a5694380d7
Hollenite <ariyanpradhan112@gmail.com>
2026-03-24 16:06:55 +0530

    setup navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar applications">Applications</div>
+        <div className="navbar analysis">Analysis</div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
